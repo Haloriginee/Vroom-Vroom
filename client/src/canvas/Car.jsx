300d445d32b22ab46680d47e1a2498f7852d4ede
Kevin Hang <herezy2006@gmail.com>
2023-04-15 22:22:50 +0200

    model implemented-ish

--- client/src/canvas/Car.jsx
+++ client/src/canvas/Car.jsx
@@ -13,18 +13,160 @@ const Car = () => {
   const stickerTexture = useTexture(snap.stickerDecal);
   const fullTexture = useTexture(snap.fullDecal);
 
+  useFrame((state, delta) => easing.dampC(materials.Material.color, snap.color, 0.25, delta));
+
+  const stateString = JSON.stringify(snap);
+
   return (
 
-    <group>
+    <group
+      key={stateString}
+    >
       <mesh
         castShadow
-        //geometry={nodes.Roadster.geometry}
-        material={materials.lambert1}
-        material-rougness={1}
+        geometry={nodes.Object_5.geometry}
+        material={materials.Material}
+        material-roughness={1}
         dispose={null}
       >
 
+        {snap.isFullTexture && (
+
+          <Decal
+            position={[0.3, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={7}
+            map={fullTexture}
+          />
+        )}
+
+        {snap.isStickerTexture && (
+
+        <Decal
+          position={[0, -0.09, 0.20]}
+          rotation={[0, 0, 0]}
+          scale={0.88}
+          map={stickerTexture}
+          map-anisotropy={16}
+          depthTest={false}
+          depthWrite={true}
+        />
+        )}
       </mesh>
+      <mesh
+        castShadow
+        geometry={nodes.Object_9.geometry}
+        material={materials.Material}
+        material-roughness={1}
+        dispose={null}
+      >
+      </mesh>
+      <mesh
+        castShadow
+        geometry={nodes.Object_8.geometry}
+        material={materials.Material}
+        material-roughness={1}
+        dispose={null}
+      >
+      </mesh>
+      <mesh
+        castShadow
+        geometry={nodes.Object_7.geometry}
+        material={materials.Material}
+        material-roughness={1}
+        dispose={null}
+      >
+      </mesh>
+      <mesh
+        castShadow
+        geometry={nodes.Object_4.geometry}
+        material={materials.Material}
+        material-roughness={1}
+        dispose={null}
+      >
+      </mesh>
+      <mesh
+        castShadow
+        geometry={nodes.Object_10.geometry}
+        material={materials.Material}
+        material-roughness={1}
+        dispose={null}
+      >
+      </mesh>
+      <mesh
+        castShadow
+        geometry={nodes.Object_11.geometry}
+        material={materials.Material}
+        material-roughness={1}
+        dispose={null}
+      >
+      </mesh>
+      <mesh
+        castShadow
+        geometry={nodes.Object_12.geometry}
+        material={materials.Material}
+        material-roughness={1}
+        dispose={null}
+      >
+      </mesh>
+      <mesh
+        castShadow
+        geometry={nodes.Object_13.geometry}
+        material={materials.Material}
+        material-roughness={1}
+        dispose={null}
+      >
+      </mesh>
+      <mesh
+        castShadow
+        geometry={nodes.Object_14.geometry}
+        material={materials.Material}
+        material-roughness={1}
+        dispose={null}
+      >
+      </mesh>
+      <mesh
+        castShadow
+        geometry={nodes.Object_15.geometry}
+        material={materials.Material}
+        material-roughness={1}
+        dispose={null}
+      >
+      </mesh>
+      <mesh
+        castShadow
+        geometry={nodes.Object_59.geometry}
+        material={materials.Material}
+        material-roughness={1}
+        dispose={null}
+      >
+      </mesh>
+      <mesh
+        castShadow
+        geometry={nodes.Object_61.geometry}
+        material={materials.Material}
+        material-roughness={1}
+        dispose={null}
+      >
+      </mesh>
+      <mesh
+        castShadow
+        geometry={nodes.Object_62.geometry}
+        material={materials.Material}
+        material-roughness={1}
+        dispose={null}
+      >
+      </mesh>
+      <mesh
+        castShadow
+        geometry={nodes.Object_64.geometry}
+        material={materials.Material}
+        material-roughness={1}
+        dispose={null}
+      >
+      </mesh>
+
+
     </group>
 
   )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,19 +17,19 @@ const Home = () => {
             <img
               src="./logo.png"
               alt="logo"
-              className="w-80 h-80 object-contain"
+              className="w-20 h-20 object-contain"
             />
           </motion.header>
 
           <motion.div className="home-content" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h1 className="head-text">
-                Customized <br className="hidden xl:block" /> Awesome Roadster.
+                Customized <br className="hidden xl:block" /> Awesome <br className="hidden xl:block" /> Roadster.
               </h1>
             </motion.div>
             <motion.div {...headContentAnimation} className="flex flex-col gap-5">
               <p className="font-normal text-base text-gray-600 max-w-md">
-                text here
+                Upgrade your Honda Civic's look with our body customization options. Our customization app lets you experiment with different designs and colors until you find the perfect combination that matches your style. Transform your Honda Civic into a street racer that looks as good as it performs!
               </p>
 
               <CustomButton
