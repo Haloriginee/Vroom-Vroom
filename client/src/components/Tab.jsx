21e85cd3f95f517a801f5cde9078a8854d37274f
Kevin Hang <herezy2006@gmail.com>
2023-04-16 13:26:16 +0200

    displayed icons

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { useSnapshot } from 'valtio';
+import state from '../store';
 
-const Tab = () => {
+const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
+
+  const snap = useSnapshot(state);
+
+  const activeStyles = isFilterTab && isActiveTab ? { backgroundColor: snap.color, opacity: 0.5 } : { backgroundColor: "transparent", opacity: 1 };
+
   return (
-    <div>Tab</div>
+
+    <div
+      className={`tab-btn ${isFilterTab ? "rounded-full glassmorphism" : "rounded-4"} `}
+      key={tab.name}
+      style={activeStyles}
+      onClick={handleClick}
+    >
+      <img
+        src={tab.icon}
+        alt=""
+      />
+    </div>
   )
 }
 
