edfc2ace85858e9ca19e3ff833817612ea9f92fc
Kevin Hang <herezy2006@gmail.com>
2023-04-19 10:00:55 +0200

    Backend setup done-ish

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ import express from "express";
 import cors from "cors";
 import * as dotenv from "dotenv";
 
+import dalle1 from "./routes/dalle.routes.js";
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }))
+
+app.use("/api/v1/dalle", dalle1)
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Pimp My Ride !!" })
